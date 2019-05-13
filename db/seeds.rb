a03606c56d2ee4497ee0685ba14b2f25fb55d66b
David Gordon <davidgordon2211@gmail.com>
2019-05-13 15:45:47 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Mcdonalds',
+    address:      'London',
+    phone_number:  '02089581111',
+    category:      'belgian'
+  },
+  {
+    name:         'Weng Wah',
+    address:      'Manchester',
+    phone_number:  '02089582222',
+    category:      'chinese'
+  },
+  {
+    name:         'Pizza Express',
+    address:      'London',
+    phone_number:  '02089583333',
+    category:      'italian'
+  },
+  {
+    name:         'Sushi Mania',
+    address:      'Liverpool',
+    phone_number:  '02089584444',
+    category:      'japanese'
+  },
+  {
+    name:         'Belgos',
+    address:      'Manchester',
+    phone_number:  '02089585555',
+    category:      'belgian'
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
